78c856facaf0fbb546f6155007e2fe759b21b427
dotnet290320 <62850561+dotnet290320@users.noreply.github.com>
2020-05-13 21:29:41 +0300

    Add files via upload

---
+++ Simon/SimonWindow.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Simon
+{
+    partial class SimonWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.redPnl = new System.Windows.Forms.Panel();
+            this.yellowPnl = new System.Windows.Forms.Panel();
+            this.greenPnl = new System.Windows.Forms.Panel();
+            this.bluePnl = new System.Windows.Forms.Panel();
+            this.startBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // redPnl
+            // 
+            this.redPnl.BackColor = System.Drawing.Color.Red;
+            this.redPnl.Location = new System.Drawing.Point(33, 35);
+            this.redPnl.Name = "redPnl";
+            this.redPnl.Size = new System.Drawing.Size(233, 182);
+            this.redPnl.TabIndex = 0;
+            // 
+            // yellowPnl
+            // 
+            this.yellowPnl.BackColor = System.Drawing.Color.Yellow;
+            this.yellowPnl.Location = new System.Drawing.Point(288, 35);
+            this.yellowPnl.Name = "yellowPnl";
+            this.yellowPnl.Size = new System.Drawing.Size(233, 182);
+            this.yellowPnl.TabIndex = 1;
+            // 
+            // greenPnl
+            // 
+            this.greenPnl.BackColor = System.Drawing.Color.Lime;
+            this.greenPnl.Location = new System.Drawing.Point(288, 238);
+            this.greenPnl.Name = "greenPnl";
+            this.greenPnl.Size = new System.Drawing.Size(233, 182);
+            this.greenPnl.TabIndex = 3;
+            // 
+            // bluePnl
+            // 
+            this.bluePnl.BackColor = System.Drawing.Color.Blue;
+            this.bluePnl.Location = new System.Drawing.Point(33, 238);
+            this.bluePnl.Name = "bluePnl";
+            this.bluePnl.Size = new System.Drawing.Size(233, 182);
+            this.bluePnl.TabIndex = 2;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(193, 446);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(175, 75);
+            this.startBtn.TabIndex = 4;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // SimonWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 544);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.greenPnl);
+            this.Controls.Add(this.yellowPnl);
+            this.Controls.Add(this.bluePnl);
+            this.Controls.Add(this.redPnl);
+            this.Name = "SimonWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Simon Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel redPnl;
+        private System.Windows.Forms.Panel yellowPnl;
+        private System.Windows.Forms.Panel greenPnl;
+        private System.Windows.Forms.Panel bluePnl;
+        private System.Windows.Forms.Button startBtn;
+    }
+}
+
---
+++ Simon/SimonWindow.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simon
+{
+    public partial class SimonWindow : Form
+    {
+        // preferences
+        // later will be in configuration file
+        const int MAX_NUMBER_OF_RANDOM = 4;
+        const int TIME_BEFORE_CLEAR_NUMBERS = 1000;
+        const int SEQUENCE_MAX_LENGTH = 8;
+        const bool CHEAT_MODE = true;
+        const string WIN_STRING = "You won!! what a great memory!!! you're the best!";
+        const string LOOSE_STRING = "Wrong ..... sorry ....";
+        const string WRONG_NUMBER_STRING = "You chose {0} the correct number was {1}";
+        const string START_USER_INPUT_STRING = "Please start the sequence...";
+        const string DO_YOU_WANT_TO_PLAY_AGAIN_STRING = "Do you want to play again? (Y-yes, N-no)";
+
+        const int RED = 1;
+        const int YELLOW = 2;
+        const int BLUE = 3;
+        const int GREEN = 4;
+
+        int[] sequence_numbers;
+
+        Random random_generator = new Random();
+
+        public SimonWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateRandomSequence()
+        {
+            sequence_numbers = new int[SEQUENCE_MAX_LENGTH];
+
+            for (int i = 0; i < SEQUENCE_MAX_LENGTH; i++)
+            {
+                sequence_numbers[i] = random_generator.Next(1, MAX_NUMBER_OF_RANDOM + 1);
+            }
+        }
+
+        private void DrawThreeTimes()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                redPnl.BackColor = Color.Black;
+                Thread.Sleep(500);
+                redPnl.BackColor = Color.Red;
+                Thread.Sleep(500);
+            }
+        }
+
+        private void DrawEntireSequence()
+        {
+            for (int i = 0; i < sequence_numbers.Length; i++)
+            {
+                //const int RED = 1;
+                //const int YELLOW = 2;
+                //const int BLUE = 3;
+                //const int GREEN = 4;
+
+                switch (sequence_numbers[i])
+                {
+                    case RED:
+                        redPnl.BackColor = Color.Black;
+                        Thread.Sleep(500);
+                        redPnl.BackColor = Color.Red;
+                        break;
+                    case YELLOW:
+                        yellowPnl.BackColor = Color.Black;
+                        Thread.Sleep(500);
+                        yellowPnl.BackColor = Color.Yellow;
+                        break;
+                    case BLUE:
+                        bluePnl.BackColor = Color.Black;
+                        Thread.Sleep(500);
+                        bluePnl.BackColor = Color.Blue;
+                        break;
+                    case GREEN:
+                        Color saveColor = greenPnl.BackColor;
+                        greenPnl.BackColor = Color.Black;
+                        Thread.Sleep(500);
+                        greenPnl.BackColor = saveColor;
+                        break;
+                }
+
+                Thread.Sleep(500);
+            }
+
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            GenerateRandomSequence();
+
+            // generate new worker
+            //Task.Run(new Action( DrawThreeTimes ));
+
+            Task.Run(new Action(DrawEntireSequence));
+
+            // multi-threaded
+            // thread -- worker
+            // ui thread 
+
+            /*
+            for (int i = 0; i < sequence_numbers.Length; i++)
+            {
+
+            }
+            */
+        }
+    }
+}
